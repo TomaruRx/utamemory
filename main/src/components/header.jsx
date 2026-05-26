bf178bcf8f0be9dccf373707b0abf1fbb746ac80
TomaruRx <169239482+TomaruRx@users.noreply.github.com>
2026-05-26 16:38:59 +0900

    change header.jsx

--- main/src/components/header.jsx
+++ main/src/components/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import favicon from '../../../style/icon/favicon.ico';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="header">
         <h1 className="site-title">
-          <img src="../../../style/icon/favicon.ico" alt="logo" className="title-icon" />
+          <img src={favicon} alt="logo" className="title-icon" />
           <ruby>
             Uta MEMOry <rt>ウタ メモリー</rt>
           </ruby>
